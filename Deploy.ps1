007ee6972af854fe009abe55b96d6101b30053d4
Tom <tom@microsky.no>
2021-03-27 21:30:50 +0100

    gf

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,3 +1,3 @@
-$rg = 'test2'
+$rg = 'rg-adds'
 New-AzResourceGroup -Name $rg -Location 'Norway East' -Force #use this command when you need to create a new resource group for your deployment
 New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile '.\azuredeploy.json'
