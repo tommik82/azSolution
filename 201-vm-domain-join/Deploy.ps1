2479d6c50364e2698d6b72d01dae70250739c5a2
Tom <tom@microsky.no>
2021-03-27 21:30:20 +0100

    gf

---
+++ 201-vm-domain-join/Deploy.ps1
@@ -0,0 +1,3 @@
+$rg = 'file01'
+New-AzResourceGroup -Name $rg -Location 'Norway East' -Force #use this command when you need to create a new resource group for your deployment
+New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile '.\azuredeploy.json'
